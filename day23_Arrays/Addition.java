91724ffe41841036c0f8c6916b6d422e17dfc767
rajmi <rajminakarmi26@gmail.com>
2020-08-03 14:56:25 -0400

    add checking.java

--- day23_Arrays/Addition.java
+++ day23_Arrays/Addition.java
@@ -4,7 +4,7 @@ package day23_Arrays;
                     if the answer is yes, repeat the whole process again
                     if the answer is no, only return the addition
                     but while the anser is not a valid answer (not yes or no), the the program should repeatedly ask the user re-enter
-            HINT: nested loop task, outter loop need to be an infinite loop
+         //   HINT: nested loop task, outter loop need to be an infinite loop
 
  */
 
